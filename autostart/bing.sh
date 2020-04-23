e1a01ac124d9626e0d95c7c7e682f1065f750f38
Ben Carlisle <bencarlisle15@gmail.com>
2020-04-22 21:10:51 -0400

    Fixed wallpaper

--- autostart/bing.sh
+++ autostart/bing.sh
@@ -1,7 +1,14 @@
+{ gsettings monitor org.gnome.desktop.background picture-uri |
+  while read x; do
+    case "$x" in
+      *"/usr/share/"*) bing-wallpaper &;;
+   esac
+  done
+} &
 { dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
   while read x; do
     case "$x" in
-      *"boolean false"*) bing-wallpaper &;;  
+      *"boolean false"*) bing-wallpaper &;;
     esac
   done
 } &
